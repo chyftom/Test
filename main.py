c40a9ab1d73361198e05c40ecbb190bf90c9746d
chyftom <chyftom@qq.com>
2024-08-14 18:08:19 +0800

    fix upload faild

---
+++ main.py
@@ -0,0 +1,35 @@
+from asyncore import loop
+from typing import Union
+
+from fastapi import FastAPI
+import datetime
+import time
+
+
+def time_printer():
+    now = datetime.datetime.now()
+    ts = now.strftime('%Y-%m-%d %H:%M:%S')
+    print('do func time :', ts)
+
+
+app = FastAPI()
+
+
+def loop_monitor():
+    while True:
+        time_printer()
+        time.sleep(5)
+
+
+@app.get("/")
+def read_root():
+    time_printer()
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+# loop_monitor()
